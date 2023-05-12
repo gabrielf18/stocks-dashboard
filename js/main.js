f742612c2b36f0c7cf637154107cf5f2162cf143
Gabriel <gabrielfserrano0@gmail.com>
2023-05-11 22:21:51 -0300

    sss

--- js/main.js
+++ js/main.js
@@ -22,8 +22,18 @@ const handleAddTicker = async (event) => {
         const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=2DK6K8A5A8UIYIT2`) //faz a requisição no API
         const data = await response.json() // transforma a resposta JSON em objeto
         console.log(data)
+        const price = data["Global Quote"]["05. price"]
         if(data["Global Quote"]["05. price"]){
-            alert('Deu certo!')
+            const newTicker = ` 
+            <div class="ticker">
+                <h2>${ticker}</h2>
+                <p>${price}</p>
+            </div>             
+            `
+
+            const tickerList = document.querySelector('#ticker-list')
+            tickerList.innerHTML += newTicker
+            closeModal('#add-stock')
         }else{
             alert(`Ticker ${ticker} não encontrado!`)
         }
